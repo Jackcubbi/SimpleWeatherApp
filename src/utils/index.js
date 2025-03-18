6250012c3daf1c543fa32c6a0e43ffee29563809
Jackcubbi <jackcubbi@gmail.com>
2025-03-18 22:09:18 +0200

    add: convert hPa units to mmhg

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,16 @@
+/**
+ * First letter to UpperCase
+ */
 export const capitalizeFirstLetter = (str) => {
   if (!str) return "";
 
   return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+/**
+ * Convert hPa units to mmhg
+ */
+const Pressure_Units = 0.750062;
+export const getPressureMm = (hpa) => {
+  return Math.round(hpa * Pressure_Units);
 };
