d802eb42b229bc86b700376dcc66378719952c65
Jackcubbi <jackcubbi@gmail.com>
2025-10-13 12:00:00 +0300

    add: Footer.vue component with template

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer class="footer">
+    <div class="footer-content">
+      <p class="footer-text">Created by Anatoli Rostsin</p>
+      <p class="footer-credits">
+        Powered by
+        <a href="https://openweathermap.org/" target="_blank" rel="noopener"
+          >OpenWeatherMap API</a
+        >
+      </p>
+    </div>
+  </footer>
+</template>
